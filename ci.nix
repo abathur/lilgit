cef82c26ff3c17e06809b1099be01eba1a01a101
Travis A. Everett <travis.a.everett@gmail.com>
2023-12-10 15:14:44 -0600

    ci: remove git conf which broke performance job
    The performance job isn't a build blocker, so I didn't notice that my
    attempts to try manyFiles (9f52c3 and 82e1a44) within the Nix build
    were causing readonly and path problems.

--- ci.nix
+++ ci.nix
@@ -28,7 +28,6 @@ stdenv.mkDerivation {
   NIXPKGS="${nixpkgs}";
 
   checkPhase = ''
-    git config --file $NIXPKGS/.git/config features.manyFiles true
     mkdir $out
     patchShebangs .
     cat tests/head_nixpkgs.bats tests/repo.bash > tests/ephemeral.bats
